8debd555a7925db1c0a34db424240073d37e6889
oleksii <alexbuttko@gmail.com>
2022-12-05 09:15:21 +0200

    first commit

---
+++ source/repos/HW_7_Employes/HW_7_Employes/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace HW_7_Employes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeSalary();
+        }
+        static void EmployeeSalary()
+        {
+            string employeeName;
+            string employeeSalary;
+
+            Console.WriteLine("Enter employes amount");
+            int employeeAmount = int.Parse(Console.ReadLine());
+
+            string[][] employeeTable = new string[employeeAmount][];
+            for(int i = 0; i < employeeAmount; i++)
+            {
+                Console.WriteLine("Enter emplyee name: ");
+                employeeName = Console.ReadLine();
+
+                Console.WriteLine($"Enter {employeeName}'s salary : ");
+                int employeeSalaryy = int.Parse(Console.ReadLine());
+                if (employeeSalaryy > 0 && employeeSalaryy < 10000)
+                {
+                    employeeSalary = Convert.ToString(employeeSalaryy);
+                    employeeTable[i] = new string[] { employeeName, employeeSalary };
+                }
+                else
+                {
+                    Console.WriteLine("Salary amount is incorrect");
+                    i--;
+                }
+            }
+            for(int i = 0; i < employeeAmount; i++)
+            {
+                Console.WriteLine($"{employeeTable[i][0]} : {employeeTable[i][1]}");
+            }
+           
+        }
+    }
+}
