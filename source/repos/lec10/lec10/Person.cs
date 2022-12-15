5c3e764eca8a50d511fc3250a217932c4161d33a
oleksii <alexbuttko@gmail.com>
2022-12-15 21:24:00 +0200

    lec 10

---
+++ source/repos/lec10/lec10/Person.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lec10
+{
+    class Person : IComparable<Person>
+    {
+        public string personName;
+        public int personAge;
+        int result;
+
+        public Person(string personName, int age)
+        {
+            this.personName = personName;
+            this.personAge = age;
+        }
+        public string GetInfo()
+        {
+            return $"Person : {personName}";
+        }
+        public int CompareTo(Person other)
+        {
+            if(other is Person personToCompare)
+            {
+                if(personToCompare.personName == this.personName || personToCompare.personAge == this.personAge)
+                {
+                    return 0;
+                }
+                else if(this.personAge < personToCompare.personAge)
+                {
+                    return -1;
+                }
+                else
+                {
+                    return 1;
+                }
+            }
+            throw new ArgumentException("Isn't person class");
+        }
+        public static bool operator < (Person person1, Person person2)
+        {
+            return person1.CompareTo(person2) < 0;
+        }
+        public static bool operator > (Person person1 , Person person2)
+        {
+            return person2.CompareTo(person2) > 0;
+        }
+        public static bool operator == (Person person1, Person person2)
+        {
+            return person1.CompareTo(person2) == 0;
+        }
+        public static bool operator !=(Person person1, Person person2)
+        {
+            return person1.CompareTo(person2) != 0;
+        }
+    }
+
+}
---
+++ source/repos/lec10/lec10/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace lec10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> personList = new List<Person>();
+            personList.Add(new Person("Alex",23));
+            personList.Add(new Person("Taras", 21));
+            personList.Add(new Person("Misha", 21));
+            personList.Add(new Person("Anya", 45));
+
+            for ( int i = 0; i < personList.Count; i++ ) 
+            {
+                for(int j = i+1; j< personList.Count; j++ ) 
+                {
+                    if (personList[i] < personList[j]) 
+                    {
+                        Console.WriteLine($"{personList[i].GetInfo()} age is less than {personList[j].GetInfo()} age");
+                    }
+                    if (personList[i] > personList[j])
+                    {
+                        Console.WriteLine($"{personList[i].GetInfo()} age is greater than {personList[j].GetInfo()}");
+                    }
+                    if (personList[i] == personList[j])
+                    {
+                        Console.WriteLine($"{personList[i].GetInfo()}  name and age or just age equals {personList[j].GetInfo()}");
+                    }
+                }
+            }
+        }
+    }
+}
