ec36763315f395a6e04eb96a2bec18b864110002
oleksii <alexbuttko@gmail.com>
2022-12-18 22:49:15 +0200

    players

---
+++ source/repos/Players/Players/PController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Players
+{
+    internal class PController<T> where T : Hero 
+    {
+        public T Data;
+        public PController(T data) 
+        {
+            Data= data;
+        }
+        public void Print() { Console.WriteLine(Data); }
+        public void ForceToAttack()
+        {
+            Data.Attack();
+        }
+    }
+}
---
+++ source/repos/Players/Players/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading;
+
+namespace Players
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var warrior = new Warrior() { Name = "Udwink", Weapon = "Sword", Damage = 100 };
+            var witcher = new Witcher() { Name = "Gherolt", Weapon = "Sword", Damage = 150 };
+
+            var warriorController = new PController<Warrior>(warrior);
+            var witcherController = new PController<Witcher>(witcher);
+
+            warriorController.ForceToAttack();
+            witcherController.ForceToAttack();
+        }
+    }
+}
